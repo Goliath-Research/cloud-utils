10720a3597129609a6b631104852cb783bcb9a19
David Izada Rodriguez <davidizadar@GoliathResearch.com>
2024-10-15 14:54:44 -0400

    Rename Local to FileSystem
    It can handle local and network folders

--- source/FileSystem_Storage.py
+++ source/FileSystem_Storage.py
@@ -1,17 +1,17 @@
 import os
 from Storage import Storage
 
-class LocalStorage(Storage):
-    def read(self, path, local_path):
-        # For local storage, simply copy the file
-        if os.path.exists(local_path):
-            offset = os.path.getsize(local_path)
+class FileSystemStorage(Storage):
+    def read(self, path, filesys_path):
+        # For file system storage, simply copy the file
+        if os.path.exists(filesys_path):
+            offset = os.path.getsize(filesys_path)
         else:
             offset = 0
 
         with open(path, 'rb') as source_file:
             source_file.seek(offset)
-            with open(local_path, 'ab') as target_file:
+            with open(filesys_path, 'ab') as target_file:
                 while True:
                     chunk = source_file.read(1024 * 1024)  # Read in 1MB chunks
                     if not chunk:
